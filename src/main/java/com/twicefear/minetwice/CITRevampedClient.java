e3af213d38472f408b39e547ca7047a91f0c928c
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-23 15:23:21 +0530

    Initialize client and register resource reload listener

--- src/main/java/com/twicefear/minetwice/CITRevampedClient.java
+++ src/main/java/com/twicefear/minetwice/CITRevampedClient.java
@@ -1,9 +1,15 @@
 package com.twicefear.minetwice;
 
+import com.twicefear.minetwice.cit.CITReloadListener;
+import com.twicefear.minetwice.config.CITRevampedConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
 
 public class CITRevampedClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
+    @Override
+    public void onInitializeClient() {
+        CITRevampedConfig.load();
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new CITReloadListener());
+    }
 }
